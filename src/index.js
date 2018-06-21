4656ef9f18ec5170ebff11c5e9ba982b13f782aa
Ciaran Schutte <ciaranschutte@gmail.com>
2018-06-21 11:02:09 -0400

    added react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const title = 'home changed page';
+const App = () => (
+  <div>{title}</div>
+);
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
   document.getElementById('app')
 );
 
